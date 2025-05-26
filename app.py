bc487bdf6f7960209139dd79d845b70dd284336a
akhilcjdata <akhilcjdata@gmail.com>
2025-05-26 18:23:02 +0530

    Data Ingestion

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ import json
 import boto3
 import sys 
 import streamlit as streamlit
+from langchain_community.llms import Bedrock
+from langchain_community.embeddings import BedrockEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.chains import retrieval_qa
+from langchain_community.vectorstores import FAISS
